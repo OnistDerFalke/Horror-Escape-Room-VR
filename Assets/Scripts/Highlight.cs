187248011386944ce3c7dc1f6c48d2ec22c8c6cd
Marcin Myśliborski <75864407+OnistDerFalke@users.noreply.github.com>
2023-12-15 16:54:37 +0100

    highlights for no pickable interactables

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    [SerializeField]
+    private List<Renderer> renderers;
+
+    private List<Material> materials;
+    private Color color;
+    private void Awake()
+    {
+        materials = new List<Material>();
+        foreach (var renderer in renderers)
+            materials.AddRange(new List<Material>(renderer.materials));
+    }
+
+    private void Update()
+    {
+        var sinVal = Mathf.Sin(2f*Time.time);
+        if (sinVal > 0.8f) sinVal = 0.8f;
+        if (sinVal < 0.2f) sinVal = 0.2f;
+        color = new Color(sinVal, sinVal, sinVal);
+        ChangeHighlight();
+    }
+
+    private void ChangeHighlight()
+    {
+        foreach (var material in materials)
+        {
+            material.EnableKeyword("_EMISSION");
+                material.SetColor("_EmissionColor", color);
+        }
+    }
+}
